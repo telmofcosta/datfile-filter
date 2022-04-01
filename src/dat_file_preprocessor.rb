4b2dffcb2de153c499c5d35ccf41947c905893bc
Telmo Costa <telmo.ferreira.costa@gmail.com>
2022-04-01 18:31:45 +0100

    Rename preprocessor parser

--- src/dat_file_preprocessor.rb
+++ src/dat_file_preprocessor.rb
@@ -2,10 +2,10 @@
 
 require "ox"
 
-# Initial Ox::Sax xml machines parser
+# Initial Ox::Sax xml datfile parser
 # After using, data is available on the {data} attribute of the instance
-class DatFileParser < Ox::Sax
-  MACHINE_PATH = %i[mame machine].freeze
+class DatFilePreprocessor < Ox::Sax
+  GAME_PATH = %i[datafile game].freeze
   DATA_ATTRS = %i[name cloneof romof].freeze
 
   attr_reader :data
@@ -21,18 +21,22 @@ class DatFileParser < Ox::Sax
     @path.length
   end
 
+  ##
+  # Events: start
+  ##
   def instruct(_target)
     @instruct = true
   end
 
-  # don't know what this is
-  def attr(name, str); end
+  # def end_instruct(target)
+  #   warn "[EI] #{indent}#{target}"
+  #   target_type.set_element
+  # end
 
   def attr_value(name, value)
     return unless @element
 
     @element[name] = value.as_s if DATA_ATTRS.include?(name)
-    $stderr.print("\r#{value.as_s[0]}") if name == :name
   end
 
   def attrs_done
@@ -42,12 +46,16 @@ class DatFileParser < Ox::Sax
 
   def start_element(name)
     @path.push(name)
-    @element = Element.new if @path == MACHINE_PATH
+    @element = Element.new if @path == GAME_PATH
   end
 
   def end_element(_name)
     @path.pop
   end
+
+  ##
+  # Events: end
+  ##
 
   Element = Struct.new(:name, :cloneof, :romof, keyword_init: true)
 end
